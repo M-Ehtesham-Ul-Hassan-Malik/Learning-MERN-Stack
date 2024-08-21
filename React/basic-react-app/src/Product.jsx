3e1f8e61b15bd1005ef9eb94cb33e66d580e9a17
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-08-21 18:27:14 +0500

    style Product component

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -1,6 +1,8 @@
+import "./Product.css"
+
 function Product(){
     return(
-        <div>
+        <div className="Product">
         <h3>Product Name</h3>
         <h5>Product Description</h5>
         </div>
