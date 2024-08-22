8880c56b16b2f0461f87197abc57c33c8e5a81d5
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-08-22 11:51:18 +0500

    learn and implement dynamic component styling, also improve the code

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -3,15 +3,17 @@ import "./Product.css"
 //props is same as aurgument in JavaScript
 function Product({title, Price, features}){
     // const list = features.map((feature) => <li>{feature}</li>);
+    let isDiscount = Price > 30000;
+    let styles = {backgroundColor : isDiscount ? "pink" : null} 
     return(
-    <div className="Product">
+    <div className="Product" style={styles} >
         <h3>{title}</h3>
         <h5>Price: {Price}</h5>
-        <h6>{features}</h6>
+        {/* <h6>{features}</h6> */}
         {/* Conditiion => {Price>30000 ? <p>Discount of 5%</p> : null}*/}
 
         {/* condition */}
-        {Price>30000 && <p>Discount of 5%</p>}
+        {isDiscount && <p>Discount of 5%</p>} 
 
     </div>
     );
