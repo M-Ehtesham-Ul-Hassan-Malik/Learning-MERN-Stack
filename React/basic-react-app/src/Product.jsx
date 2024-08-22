f0a92f654ba8dfe673c53094a395ceeb8aa1f091
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-08-22 11:42:54 +0500

    learn and implement condition in react

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -2,12 +2,18 @@ import "./Product.css"
 
 //props is same as aurgument in JavaScript
 function Product({title, Price, features}){
+    // const list = features.map((feature) => <li>{feature}</li>);
     return(
-        <div className="Product">
+    <div className="Product">
         <h3>{title}</h3>
         <h5>Price: {Price}</h5>
         <h6>{features}</h6>
-        </div>
+        {/* Conditiion => {Price>30000 ? <p>Discount of 5%</p> : null}*/}
+
+        {/* condition */}
+        {Price>30000 && <p>Discount of 5%</p>}
+
+    </div>
     );
 }
 
--- React/basic-react-app/src/ProductTab.jsx
+++ React/basic-react-app/src/ProductTab.jsx
@@ -1,9 +1,11 @@
 import Product from "./Product";
 
 function ProductTab(){
+    let options = [<li>High-Tech</li>,<li>Durable</li>,<li>Fast</li>];
+    
     return(
         <>
-        <Product title="Mobile Phone" Price={40000} features={["High-Tech, ", "Durable, ", "Fast"]}/>
+        <Product title="Mobile Phone" Price={40000} features={options}/>
         <Product title="Laptop" Price={59999}/>
         <Product title="Pen" Price={40}/>
         </>
