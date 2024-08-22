177c951e0b43f80a6cd2003c205a12185e929eb9
M Ehtesham Ul Hassan Malik <malikehtesham.ths@gmail.com>
2024-08-22 10:49:41 +0500

    learn to pass array into props in react

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -1,11 +1,12 @@
 import "./Product.css"
 
 //props is same as aurgument in JavaScript
-function Product({title, Price}){
+function Product({title, Price, features}){
     return(
         <div className="Product">
         <h3>{title}</h3>
         <h5>Price: {Price}</h5>
+        <h6>{features}</h6>
         </div>
     );
 }
--- React/basic-react-app/src/ProductTab.jsx
+++ React/basic-react-app/src/ProductTab.jsx
@@ -3,7 +3,7 @@ import Product from "./Product";
 function ProductTab(){
     return(
         <>
-        <Product title="Mobile Phone" Price={40000}/>
+        <Product title="Mobile Phone" Price={40000} features={["High-Tech, ", "Durable, ", "Fast"]}/>
         <Product title="Laptop" Price={59999}/>
         <Product title="Pen" Price={40}/>
         </>
